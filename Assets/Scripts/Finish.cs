d61f4b6b03a9497a65eb52969f507650e2106af7
Marcos202120 <78693710+Marcos202120@users.noreply.github.com>
2021-02-21 18:12:39 -0800

    Se

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+	// When an object enters the finish zone, let the
+	// game manager know that the current game has ended
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player")) ;
+		{
+			UnityEngine.SceneManagement.SceneManager.LoadScene("End");
+		}
+	}
+}
